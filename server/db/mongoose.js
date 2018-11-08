0bb293aa5a5d4c77c05b23146d56badee5ee26ba
ZaitcevIlya <intiguan@yahoo.com>
2018-11-09 02:10:24 +0300

    update db uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dbURI =  'mongodb://<dbuser>:<dbpassword>@ds155833.mlab.com:55833/todo_app' || 'mongodb://localhost:27017/TodoApp'
+const dbURI =  'mongodb://<admin>:<123qwe>@ds155833.mlab.com:55833/todo_app' || 'mongodb://localhost:27017/TodoApp'
 // 'mongodb://<dbuser>:<dbpassword>@ds155833.mlab.com:55833/todo_app'
 
 
