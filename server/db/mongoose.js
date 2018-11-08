482cc03ebe051f133394734b91ec2034eb1a4f81
ZaitcevIlya <intiguan@yahoo.com>
2018-11-09 02:26:25 +0300

    fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const dbURI =  'mongodb://<admin>:<123qwe>@ds155833.mlab.com:55833/todo_app' || 'mongodb://localhost:27017/TodoApp'
-// 'mongodb://<dbuser>:<dbpassword>@ds155833.mlab.com:55833/todo_app'
+const dbURI =  'mongodb://admin:123qwe@ds155833.mlab.com:55833/todo_app' || 'mongodb://localhost:27017/TodoApp'
+// 'mongodb://admin:123qwe@ds155833.mlab.com:55833/todo_app'
 
 
 mongoose.Promise = global.Promise
